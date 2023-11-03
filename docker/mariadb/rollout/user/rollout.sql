9e3786adcd592814ef16a7c5a70ceff8780b876f
m.zherlygin <m.zherlygin@arenadata.io>
2023-11-03 15:33:16 +0300

    test

--- docker/mariadb/rollout/user/rollout.sql
+++ docker/mariadb/rollout/user/rollout.sql
@@ -4,7 +4,7 @@ create table _roles (
     caption varchar(64) not null,
     PRIMARY KEY(id)
 );
-insert into _roles (name, caption) values ("admin", "Admin role");
+insert into _roles (name, caption) values ("admin", "Admin role test");
 
 create table _user (
     id INT NOT NULL, 
@@ -21,31 +21,31 @@ create table _users_roles (
     unique(role_name, user_id)
 );
 
--- insert into _user(login, password,  name, email) values ('autotest1', '123', 'Test User 1', 'email@dot1.com');
--- insert into _user(login, password,  name, email) values ('autotest2', '123', 'Test User 2', 'email@dot2.com');
--- insert into _users_roles select r.id, u.id from _user u, _roles r where u.login = 'autotest1' and r.name = 'admin';
+insert into _user(login, password,  name, email) values ('autotest1', '123', 'Test User 1', 'email@dot1.com');
+insert into _user(login, password,  name, email) values ('autotest2', '123', 'Test User 2', 'email@dot2.com');
+insert into _users_roles select r.id, u.id from _user u, _roles r where u.login = 'autotest1' and r.name = 'admin';
 
--- create table _product (
---     id INT NOT NULL AUTO_INCREMENT,
---     name varchar(256) not null,
---     description varchar(512) not null,
---     cost float not null,
---     seller_id int not null references _user(id),
---     creation_date DATETIME not null default now(),
---     deleted boolean not null default false,
---     PRIMARY KEY(id)
--- );
--- insert into _product (name, description, cost, seller_id) 
---     select
---         "test product 1",
---         "test product 1",
---         100,
---         u.id
---     from _user as u where login = 'autotest1';
--- insert into _product (name, description, cost, seller_id) 
---     select
---         "test product 2",
---         "test product 2",
---         500,
---         u.id
---     from _user as u where login = 'autotest2';
+create table _product (
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(256) not null,
+    description varchar(512) not null,
+    cost float not null,
+    seller_id int not null references _user(id),
+    creation_date DATETIME not null default now(),
+    deleted boolean not null default false,
+    PRIMARY KEY(id)
+);
+insert into _product (name, description, cost, seller_id) 
+    select
+        "test product 1",
+        "test product 1",
+        100,
+        u.id
+    from _user as u where login = 'autotest1';
+insert into _product (name, description, cost, seller_id) 
+    select
+        "test product 2",
+        "test product 2",
+        500,
+        u.id
+    from _user as u where login = 'autotest2';
