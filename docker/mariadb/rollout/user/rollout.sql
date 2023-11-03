8256ebe231f65e82729f1c523e4e58ce5e4c3392
m.zherlygin <m.zherlygin@arenadata.io>
2023-11-03 16:16:14 +0300

    added debug logs

--- docker/mariadb/rollout/user/rollout.sql
+++ docker/mariadb/rollout/user/rollout.sql
@@ -1,3 +1,4 @@
+
 create table _roles (
     id INT NOT NULL AUTO_INCREMENT, 
     name varchar(32) not null unique,
@@ -20,11 +21,6 @@ create table _users_roles (
     user_id int references _user(id),
     unique(role_name, user_id)
 );
-
-insert into _user(login, password,  name, email) values ('autotest1', '123', 'Test User 1', 'email@dot1.com');
-insert into _user(login, password,  name, email) values ('autotest2', '123', 'Test User 2', 'email@dot2.com');
-insert into _users_roles select r.id, u.id from _user u, _roles r where u.login = 'autotest1' and r.name = 'admin';
-
 create table _product (
     id INT NOT NULL AUTO_INCREMENT,
     name varchar(256) not null,
@@ -35,6 +31,11 @@ create table _product (
     deleted boolean not null default false,
     PRIMARY KEY(id)
 );
+
+insert into _user(login, password,  name, email) values ('autotest1', '123', 'Test User 1', 'email@dot1.com');
+insert into _user(login, password,  name, email) values ('autotest2', '123', 'Test User 2', 'email@dot2.com');
+insert into _users_roles select r.id, u.id from _user u, _roles r where u.login = 'autotest1' and r.name = 'admin';
+
 insert into _product (name, description, cost, seller_id) 
     select
         "test product 1",
