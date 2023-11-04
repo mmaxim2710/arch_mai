c973995f1fb2dedd16f44bedb2afa92d7a3d9c42
m.zherlygin <m.zherlygin@arenadata.io>
2023-11-04 21:08:01 +0300

    up id of admins

--- docker/mariadb/rollout/user/rollout.sql
+++ docker/mariadb/rollout/user/rollout.sql
@@ -32,8 +32,8 @@ create table _product (
     PRIMARY KEY(id)
 );
 
-insert into _user(id, login, password,  name, email) values (1, 'autotest1', '123', 'Test User 1', 'email@dot1.com');
-insert into _user(id, login, password,  name, email) values (2, 'autotest2', '123', 'Test User 2', 'email@dot2.com');
+insert into _user(id, login, password,  name, email) values (1000, 'autotest1', '123', 'Test User 1', 'email@dot1.com');
+insert into _user(id, login, password,  name, email) values (1001, 'autotest2', '123', 'Test User 2', 'email@dot2.com');
 -- insert into _users_roles select r.id, u.id from _user u, _roles r where u.login = 'autotest1' and r.name = 'admin';
 insert into _users_roles(role_name, user_id) values ('admin', 1);
 insert into _users_roles(role_name, user_id) values ('admin', 2);
